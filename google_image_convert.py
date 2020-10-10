53c06ad7f78e40d8630089130fcb7dda2ca307c7
Jessica Gaoyuan Liang <gaoyuanliang@outlook.com>
2020-10-11 00:37:38 +0400

    Update google_image_convert.py

--- google_image_convert.py
+++ google_image_convert.py
@@ -1,14 +1,28 @@
 import re
 import os
+from PIL import Image
 
 index = 1
-image_dir = u"road_sign_abu_dhabi_files"
+image_dir = u"adnoc_station_files"
 
 while True:
-	f = "{}/images\({}\)".format(image_dir, index)
-	f_new = "{}/{}_{}.jpg".format(image_dir, image_dir, index)
-	cmd = "mv {} {}".format(f,f_new)
-	cmd_result = os.system(cmd)
-	index += 1
-	if cmd_result != 0:
+	try:
+		f = "{}/images\({}\)".format(image_dir, index)
+		im=Image.open("{}/images({})".format(image_dir, index))
+		f_new = "{}/{}_{}.{}".format(image_dir, image_dir, index, im.format)
+		cmd = "mv {} {}".format(f,f_new)
+		cmd_result = os.system(cmd)
+		index += 1
+		if cmd_result != 0:
+			break
+	except:
 		break
+
+
+dirs = os.listdir( image_dir )
+
+for files in dirs:
+	try:
+		print(re.search(r'^.*\.json$', files).group())
+	except:
+		pass
